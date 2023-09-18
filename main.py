f57fea07c374dfa60a0615b14fa92ee1920d6bcb
Ankit Yadav <96226262+virtualknight8@users.noreply.github.com>
2023-09-18 21:42:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-    return 'kuch bhi b'
-
+   print('kuch bhi')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
