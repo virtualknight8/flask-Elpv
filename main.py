807eaf7157cde46a63f2288fd0af4130daf85937
Ankit Yadav <96226262+virtualknight8@users.noreply.github.com>
2023-09-20 00:07:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ postgres_password = os.environ.get('PGPASSWORD')
 @app.route('/')
 def index():
   # Create a connection to the PostgreSQL database
-  try:
+    try:
     conn = psycopg2.connect(
         host=postgres_host,
         port=postgres_port,
@@ -29,17 +29,10 @@ def index():
     # Close the connection
     conn.close()
 
-except psycopg2.Error as e:
+    except psycopg2.Error as e:
     # If there's an error, print an error message
     print("Error connecting to PostgreSQL:", e)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
-
-
-
-
-
-
-
 
