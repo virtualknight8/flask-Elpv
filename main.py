3410bd0e2d1bea02ccac4dbf262234a840e92743
Ankit Yadav <96226262+virtualknight8@users.noreply.github.com>
2023-09-19 23:56:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import os
 app = Flask(__name__)
 
 postgres_host = os.environ.get('PGHOST')
+purl = os.environ.get('DATABASE_URL')
 
 @app.route('/')
 def index():
-   return postgres_host
-   return 'kuch bhi'
+   return { 'host' : postgres_host,
+'url' : purl}
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
