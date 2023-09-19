a40374e658b40ebc5d318c3f0d8ff31f925dcb9f
Ankit Yadav <96226262+virtualknight8@users.noreply.github.com>
2023-09-19 23:37:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ postgres_host = os.environ.get('PGHOST')
 @app.route('/')
 def index():
    return postgres_host
+   return 'kuch bhi'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
